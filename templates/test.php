7fa481e7d2a7faeafb1f403244733119498fd528
danielaridel <46938729+danielaridel@users.noreply.github.com>
2021-03-02 17:48:21 -0300

    Update test.php

--- templates/test.php
+++ templates/test.php
@@ -1,7 +1,8 @@
 <?php
   // The global $_POST variable allows you to access the data sent with the POST method
   // To access the data sent with the GET method, you can use $_GET
-  $say = htmlspecialchars($_GET['say']);
-  $to  = htmlspecialchars($_GET['to']);
+  $say = $_GET['say'];
+  $to  = $_GET['to'];
 
   echo  $say, ' ', $to;
+?>
