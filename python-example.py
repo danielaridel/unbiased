70c870afc74ab1ddec22d306a1e4248788e35d67
danielaridel <46938729+danielaridel@users.noreply.github.com>
2021-03-02 15:20:13 -0300

    Update python-example.py

--- python-example.py
+++ python-example.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def form():
     return render_template('form.html')
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 def hello():
     return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
 
